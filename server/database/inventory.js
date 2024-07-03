0418409d866f91b4e4efd85e4c22c1f9a2140124
Daniel Zorrilla <95542619+dfzorrilla@users.noreply.github.com>
2024-07-04 00:54:16 +0200

    Update inventory.js

--- server/database/inventory.js
+++ server/database/inventory.js
@@ -1,33 +1,33 @@
-const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const cars = new Schema({
-dealer_id: {
-    type: Number,
-    required: true
-},
-make: {
-    type: String,
-    required: true
-  },
-model: {
-    type: String,
-    required: true
-  },
-bodyType: {
-    type: String,
-    required: true
-  },
-year: {
-    type: Number,
-    required: true
-  },
-mileage: {
-    type: Number,
-    required: true
-  }
+const carSchema = new Schema({
+    dealerId: {
+        type: Number,
+        required: true
+    },
+    make: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    bodyType: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    mileage: {
+        type: Number,
+        required: true
+    }
 });
 
-module.exports = mongoose.model('cars', cars);
+// Export the model
+module.exports = mongoose.model('Car', carSchema);
