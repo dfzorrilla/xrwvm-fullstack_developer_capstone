22c7cfbfc40643a6ec34f6186d268d1f039b224e
Daniel Zorrilla <95542619+dfzorrilla@users.noreply.github.com>
2024-07-04 00:09:14 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,8 @@
 # Uncomment the following imports before adding the Model code
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib import admin  # Import this at the top of the file
+from .models import CarMake, CarModel  # Import this at the top of the file
 
 
 # Create your models here.
@@ -21,13 +23,16 @@ class CarModel(models.Model):
     )  # Many-to-One relationship
 
     name = models.CharField(max_length=100)
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
+
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+
     year = models.IntegerField(
         default=2023,
         validators=[
@@ -40,9 +45,6 @@ class CarModel(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-
-from django.contrib import admin
-from .models import CarMake, CarModel
 
 # Registering models with their respective admins
 admin.site.register(CarMake)
